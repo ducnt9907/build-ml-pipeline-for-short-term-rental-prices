e553b8847c51a5ae793955bdf01881f2b6c17502
ducnt9907 <ducnt9907@gmail.com>
2024-10-13 05:29:42 +0700

    submission

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -1,7 +1,13 @@
+import argparse
+import logging
+import wandb
 import pandas as pd
 import numpy as np
 import scipy.stats
 
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 def test_column_names(data):
 
@@ -60,6 +66,72 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all()
+
+
+def go(args):
+
+    run = wandb.init(job_type="data_check")
+    run.config.update(args)
+
+    csv_path = run.use_artifact(args.csv).file()
+    data = pd.read_csv(csv_path)
+
+    ref_path = run.use_artifact(args.ref).file()
+    ref_data = pd.read_csv(ref_path)
+
+    test_column_names(data)
+    test_neighborhood_names(data)
+    test_proper_boundaries(data)
+    test_similar_neigh_distrib(data, ref_data, args.kl_threshold)
+    test_row_count(data)
+    test_price_range(data, args.min_price, args.max_price)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Check dataset")
+
+
+    parser.add_argument(
+        "--csv", 
+        type=str,
+        help="dataset",
+        required=True
+    )
+
+    parser.add_argument(
+        "--ref", 
+        type=str,
+        help="reference dataset",
+        required=True
+    )
+
+    parser.add_argument(
+        "--kl_threshold", 
+        type=float,
+        help="Threshold for the KL divergence test on the neighborhood group column",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum accepted price",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="Maximum accepted price",
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
